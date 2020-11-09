9d3bfa875580d9d85479f4861c566e69bb1f1eaa
Diego Miguel <dml1001@alu.ubu.es>
2020-11-09 22:13:27 +0100

    Implement Post-processor (#24)

---
+++ src/text_postprocessing.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 Diego Miguel Lozano <dml1001@alu.ubu.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# For license information on the libraries used, see LICENSE.
+
+"""Text post-processor class."""
+
+from utils.tokenization import sentence_tokenize
+from truecase import get_true_case
+
+class TextPostprocessor:
+    """Text post-processing utilities.
+
+    This post-processor carries out the following preprocessing tasks over
+    the texts:
+    - Formats the text correctly (e.g., removes incorrect whitespaces).
+    - Performs truecasing over the text.
+    """
+
+    def postprocess(self, text: str) -> str:
+        """Post-process the text.
+        
+        Args:
+            text:
+                Text to be post-processed.
+
+        Returns:
+            Post-processed text as a string.
+        """
+
+        txt = ' '.join(sentence_tokenize(text))
+        return get_true_case(txt)
